f9cc8b66688336e1ae1bdeb6ded1b6bde363ed38
github username <github email address>
2019-02-25 15:34:56 -0500

    serializers

--- app/serializers/court_serializer.rb
+++ app/serializers/court_serializer.rb
@@ -1,4 +1,4 @@
 class CourtSerializer < ActiveModel::Serializer
   attributes :id, :address, :name, :longitude, :latitude
-  has_many :user_courts
+  has_many :users
 end
--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,4 +1,4 @@
 class UserSerializer < ActiveModel::Serializer
   attributes :id, :user_name, :full_name, :password, :active
-  has_many :user_courts
+  has_many :courts
 end

4be37d141d1a4709f6c27de6e7616d9f34b2bc53
github username <github email address>
2019-02-27 17:20:19 -0500

    /signin route

--- app/controllers/user_courts_controller.rb
+++ app/controllers/user_courts_controller.rb
@@ -1,2 +1,24 @@
 class UserCourtsController < ApplicationController
+
+  def index
+    render json: UserCourt.all
+  end
+
+  def create
+    @user = UserCourt.find_by(user_id: params["user_id"], court_id: params["court_id"])
+    if !@user
+      @usercourt = UserCourt.create(user_id: params["user_id"], court_id: params["court_id"])
+      render json: {user: @usercourt.user, error: false}
+    else
+      render json: {user: @user, error: true}
+    end
+  end
+
 end
+
+# if !@user
+#   @usercourt = UserCourt.create(user_id: params["user_id"], court_id: params["court_id"] )
+#   render json: @usercourt.user
+# else
+#   render json: @user
+# end
--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,6 @@ Rails.application.routes.draw do
   resources :users, only: [:index, :show]
   post '/signin', to: 'users#signin'
   resources :courts, only: [:index, :show]
+  resources :user_courts
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
